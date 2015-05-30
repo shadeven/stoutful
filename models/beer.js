8dbfae966d22b02fc6ad22973cb953585675e521
Alex Fu <alex.fu914@gmail.com>
2015-05-30 19:35:29 -0400

    Update elasticsearch index after beer has been saved.

--- models/beer.js
+++ models/beer.js
@@ -1,11 +1,33 @@
 var database = require('../database');
 var bookshelf = require('bookshelf')(database);
+var elasticsearch = require('../elasticsearch');
 
 var Brewery = require('./brewery');
 
 module.exports = bookshelf.Model.extend({
   tableName: 'beers',
+  initialize: function () {
+    // Update elasticsearch index after save.
+    this.on('saved', this.index);
+  },
   brewery: function() {
     return this.belongsTo(Brewery);
+  },
+  index: function () {
+    elasticsearch.update({
+      index: 'stoutful',
+      type: 'beer',
+      id: model.get('id'),
+      body: {
+        upsert: {
+          name: model.get('name'),
+          description: model.get('description')
+        }
+      }
+    }, function(err) {
+      if (err) {
+        console.log('Unable to index user');
+      }
+    });
   }
 });
