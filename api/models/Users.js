9fca72d23500c2d6e8ba73258d818b3e54b0e656
Alex Fu <alex.fu914@gmail.com>
2015-06-15 07:07:26 -0400

    Rename user model/controller to be plural.

--- api/models/Users.js
+++ api/models/Users.js
@@ -1,5 +1,5 @@
 /**
-* User.js
+* Users.js
 *
 * @description :: TODO: You might write a short summary of how this model works and what it represents here.
 * @docs        :: http://sailsjs.org/#!documentation/models
@@ -8,16 +8,29 @@
 module.exports = {
 
   attributes: {
+    id: {
+      type: 'integer',
+      primaryKey: true,
+      autoIncrement: true
+    },
     email: {
       type: 'email',
       notNull: true,
       unique: true,
       required: true
     },
-    firstName: {
+    first_name: {
       type: 'string',
       notNull: true,
       required: true
+    },
+    last_name: {
+      type: 'string',
+      notNull: true,
+      required: true
+    },
+    image_url: {
+      type: 'string'
     }
   }
 };
