a62659c13174eb7f84c96c8e00470226c7bf7cb1
Alex Fu <alex.fu914@gmail.com>
2015-06-21 10:59:14 -0400

    Handle refresh_token grant type.

--- api/controllers/AuthController.js
+++ api/controllers/AuthController.js
@@ -1,54 +1,133 @@
 var Promise = require('bluebird');
 var bcrypt = require('bcrypt');
 var crypto = require('crypto');
+var moment = require('moment');
 
 module.exports = {
   token: function(req, res) {
-    var body = req.body;
-    var email = body.email;
-    var password = body.password;
+    var grantType = req.body.grant_type;
 
-    // Request requires an email and password
-    if (!email && !password) {
+    if (!grantType) {
       res.status(400).end();
+      return;
     }
 
-    // Find user in database and generate an access token.
-    findUser(email)
-      .then(function(user) {
-        this.sails.models.accesstoken.findOne({user_id: user.id}, function(err, accessToken) {
-          if (err) {
-            res.status(500).end();
-            return;
-          }
-          if (accessToken) {
-            // We already have an access token for this user.
-            res.status(200).json(accessToken);
-          } else {
-            // We don't already have an access token for this user.
-            verifyCredentials(user, password)
-              .then(generateAccessToken)
-              .then(generateRefreshToken)
-              .then(saveRefreshToken)
-              .then(saveAccessToken)
-              .then(function(token) {
-                // Reformat this token to comply with OAuth2 spec
-                delete token.user;
-                res.status(201).json(token);
-              })
-              .catch(function(err) {
-                console.log(err);
-                res.status(500).end();
-              });
-          }
-        });
-      })
-      .catch(function(err) {
-        console.log(err);
-        res.status(500).end();
-      });
+    if (grantType == 'password') {
+      handlePasswordGrant(req, res);
+    } else if (grantType == 'refresh_token') {
+      handleRefreshTokenGrant(req, res);
+    }
   }
 };
+
+/**
+ * Handles a refresh token grant
+ */
+function handleRefreshTokenGrant(req, res) {
+  var body = req.body;
+  var refreshToken = body.refresh_token;
+
+  // A refresh token is needed in order to refresh a users token.
+  if (!refreshToken) {
+    res.status(400).end();
+  }
+
+  // Authenticate client?
+
+  // Validate refresh token
+  var RefreshToken = this.sails.models.refreshtoken;
+  RefreshToken.findOne({ token: refreshToken })
+    .then(function(result) {
+      if (!result) {
+        res.status(401).end();
+        return;
+      }
+
+      // Issue a new access token
+      generateAccessToken({ id: result.user_id })
+        .then(saveAccessToken)
+        .then(function(token) {
+          // Reformat this token to comply with OAuth2 spec
+          delete token.user;
+          res.status(201).json(token);
+        })
+        .catch(function(err) {
+          console.log('Error issuing access token: ', err);
+          res.status(500).end();
+        });
+    });
+}
+
+/**
+ * Handles a password credentials grant
+ */
+function handlePasswordGrant(req, res) {
+  var body = req.body;
+  var email = body.username;
+  var password = body.password;
+
+  // Request requires an email and password
+  if (!email || !password) {
+    res.status(400).end();
+    return;
+  }
+
+  // Find user in database and generate an access token
+  findUser(email)
+    .then(function(user) {
+      if (!user) {
+        // The user doesnt exist in our system
+        res.status(401).end();
+        return;
+      }
+
+      var AccessToken = this.sails.models.accesstoken;
+      var RefreshToken = this.sails.models.refreshtoken;
+      AccessToken.findOne({user_id: user.id}, function(err, accessToken) {
+        if (err) {
+          res.status(500).end();
+          return;
+        }
+        if (accessToken) {
+          // We already have an access token for this user,
+          // so we must also have a refresh token
+          RefreshToken.findOne({ user_id: accessToken.user_id })
+            .then(function(refreshToken) {
+              res.status(200).json({
+                access_token: accessToken.token,
+                refresh_token: refreshToken.token,
+                token_type: 'bearer',
+                expires_in: accessToken.expiresIn()
+              });
+            })
+            .catch(function(err) {
+              console.log('Error querying refresh token: ', err);
+              res.status(500).end();
+            });
+        } else {
+          // We don't already have an access token for this user.
+          verifyCredentials(user, password)
+            .then(generateAccessToken)
+            .then(generateRefreshToken)
+            .then(saveRefreshToken)
+            .then(saveAccessToken)
+            .then(function(token) {
+              // Reformat this token to comply with OAuth2 spec
+              delete token.user;
+              res.status(201).json(token);
+            })
+            .catch(function(err) {
+              console.log('Error issuing access token: ', err);
+              res.status(500).end();
+            });
+        }
+      });
+    })
+    .catch(function(err) {
+      console.log(err);
+      res.status(500).end();
+    });
+}
 
 /**
  * Stores given access token.
@@ -56,13 +135,13 @@ module.exports = {
 function saveAccessToken(payload) {
   return new Promise(function(fulfill, reject) {
     var AccessToken = this.sails.models.accesstoken;
-
-    AccessToken.create({ user_id: payload.user.id, token: payload.token })
+    var expiresAt = moment().add(24, 'hours').toDate();
+    AccessToken.create({ user_id: payload.user.id, token: payload.access_token, expires_at: expiresAt })
       .then(function(accessToken) {
         // Apply a TTL to the access token.
         AccessToken.native(function(err, redis) {
           var key = 'waterline:accesstoken:id:' + accessToken.id;
-          var expiresIn = 24 * (60*60); // 24 hours
+          var expiresIn = accessToken.expiresIn();
           redis.expire([key, expiresIn], function(err, success) {
             if (err) reject(err);
             if (success) {
@@ -106,11 +185,11 @@ function generateRefreshToken(payload) {
  * Generates an access token for the given user.
  */
 function generateAccessToken(user) {
+  var AccessToken = this.sails.models.accesstoken;
   return new Promise(function(fulfill) {
-    var prefix = crypto.randomBytes(4).toString('hex');
-    var body = crypto.randomBytes(60).toString('hex');
+    var token = AccessToken.generate();
     var type = 'bearer';
-    fulfill({user: user, access_token: prefix + '.' + body, token_type: type});
+    fulfill({user: user, access_token: token, token_type: type});
   });
 }
 
--- api/models/AccessToken.js
+++ api/models/AccessToken.js
@@ -4,6 +4,8 @@
 * @description :: TODO: You might write a short summary of how this model works and what it represents here.
 * @docs        :: http://sailsjs.org/#!documentation/models
 */
+var crypto = require('crypto');
+var moment = require('moment');
 
 module.exports = {
   connection: 'redis',
@@ -11,10 +13,24 @@ module.exports = {
   autoUpdatedAt: false,
   attributes: {
     user_id: {
-      type: 'integer'
+      type: 'integer',
+      required: true
     },
     token: {
-      type: 'string'
+      type: 'string',
+      required: true
+    },
+    expires_at: {
+      type: 'datetime',
+      required: true
+    },
+    expiresIn: function() {
+      return parseInt(moment.duration(moment(this.expires_at).diff(moment())).asSeconds());
     }
+  },
+  generate: function() {
+    var prefix = crypto.randomBytes(4).toString('hex');
+    var body = crypto.randomBytes(60).toString('hex');
+    return prefix + '.' + body;
   }
 };
