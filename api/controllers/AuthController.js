df6ee1fa6020680abe1f5d8ae27f52f9f53ee71c
Alex Fu <alex.fu914@gmail.com>
2015-06-21 07:51:05 -0400

    Generate RefreshToken when issuing a new AccessToken.

--- api/controllers/AuthController.js
+++ api/controllers/AuthController.js
@@ -1,7 +1,9 @@
-/* global sails */
-
 var Promise = require('bluebird');
 var bcrypt = require('bcrypt');
+var jwt = require('jsonwebtoken');
+
+var CLIENT_ID = "$2a$08$y9XgmyXZvOS0ulJHz1ZH0eLcjegUwoKLc2sss0beebAD/wYtWF22O";
+var CLIENT_SECRET = "$2a$10$WGKsiK58U11EdeMJWMmoHOZoScMQ7qvGJixNH6Z9cWEu62M3sD9N6";
 
 module.exports = {
   token: function(req, res) {
@@ -29,6 +31,8 @@ module.exports = {
             // We don't already have an access token for this user.
             verifyCredentials(user, password)
               .then(generateAccessToken)
+              .then(generateRefreshToken)
+              .then(saveRefreshToken)
               .then(saveAccessToken)
               .then(setAccessTokenTTL)
               .then(function(token) {
@@ -53,12 +57,16 @@ module.exports = {
  */
 function setAccessTokenTTL(accessToken) {
   return new Promise(function(fulfill, reject) {
-    this.sails.models.accesstoken.native(function(err, redis) {
+    var AccessToken = this.sails.models.accesstoken;
+
+    AccessToken.native(function(err, redis) {
       var key = 'waterline:accesstoken:id:' + accessToken.id;
       var expiresIn = 24 * (60*60); // 24 hours
       redis.expire([key, expiresIn], function(err, success) {
         if (err) reject(err);
         if (success) {
+          accessToken.expires_in = expiresIn;
+          delete accessToken.id; // Drop access token ID as it's not relevant to the client.
           fulfill(accessToken);
         } else {
           reject(new Error('Unable to set TTL time on access token.'));
@@ -71,12 +79,43 @@ function setAccessTokenTTL(accessToken) {
 /**
  * Stores given access token.
  */
-function saveAccessToken(accessToken) {
+function saveAccessToken(payload) {
   return new Promise(function(fulfill, reject) {
-    this.sails.models.accesstoken.create({user_id: accessToken.user.id, token: accessToken.token}, function(err, token) {
-      if (err) reject(err);
-      fulfill(token);
-    });
+    var AccessToken = this.sails.models.accesstoken;
+
+    AccessToken.create({ user_id: payload.user.id, token: payload.token })
+      .then(function(accessToken) {
+        accessToken.refresh_token = payload.refresh_token;
+        fulfill(accessToken);
+      })
+      .catch(reject);
+  });
+}
+
+function saveRefreshToken(payload) {
+  return new Promise(function(fulfill, reject) {
+    var RefreshToken = this.sails.models.refreshtoken;
+
+    RefreshToken.create({ user_id: payload.user.id, token: payload.refresh_token })
+      .then(function() { fulfill(payload); })
+      .catch(reject);
+  });
+}
+
+/**
+ * Generates a refresh token for the given access token.
+ */
+function generateRefreshToken(payload) {
+  return new Promise(function(fulfill) {
+    var refresh_token = jwt.sign({
+      iss: 'stoutful',
+      aud: CLIENT_ID,
+      sub: payload.token },
+      CLIENT_SECRET,
+      {algorithm: 'HS512'});
+
+    payload.refresh_token = refresh_token;
+    fulfill(payload);
   });
 }
 
@@ -84,11 +123,9 @@ function saveAccessToken(accessToken) {
  * Generates an access token for the given user.
  */
 function generateAccessToken(user) {
-  return new Promise(function(fulfill, reject) {
-    bcrypt.hash(user.email, 10, function(err, hash) {
-      if (err) reject(err);
-      fulfill({user: user, token: hash});
-    });
+  return new Promise(function(fulfill) {
+    var token = jwt.sign({ iss: 'stoutful', aud: CLIENT_ID, sub: user.id }, CLIENT_SECRET);
+    fulfill({user: user, token: token});
   });
 }
 
