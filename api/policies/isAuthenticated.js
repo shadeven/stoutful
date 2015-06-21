43911e60aecc273ab427b5fa3b5f03d399ad9982
Alex Fu <alex.fu914@gmail.com>
2015-06-21 11:26:53 -0400

    Update `isAuthenticated` policy.

--- api/policies/isAuthenticated.js
+++ api/policies/isAuthenticated.js
@@ -1,18 +1,27 @@
 /* global sails */
 
 module.exports = function(req, res, next) {
-  var accessToken = req.headers.authorization;
+  var authorization = req.headers.authorization;
 
   // No access token no access
-  if (!accessToken) {
+  if (!authorization) {
     return res.status(401).end();
   }
 
+  // Extract token from Authorization header
+  // Expect Authorization to be "Bearer ..."
+  var regex = /^Bearer (.*)$/;
+  var match = regex.exec(authorization);
+  if (!match) {
+    return res.status(400).end();
+  }
+  var token = match[1];
+
   // Verify access token is correct
-  sails.models.accesstoken.findOne({token: accessToken}, function(err, token) {
+  sails.models.accesstoken.findOne({token: token}, function(err, result) {
     if (err) return next(err);
 
-    if (!token) {
+    if (!result) {
       return res.status(401).end();
     }
 
