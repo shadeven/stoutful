d8f49e9c94cfea9a8a6eab816a51e455b7db0751
Steven Wu <steven.j.wu@gmail.com>
2015-10-14 00:17:18 +1030

    Display user activities.

--- assets/js/controllers/profile.js
+++ assets/js/controllers/profile.js
@@ -1,5 +1,24 @@
 angular.module('stoutful.controllers').
-  controller('ProfileController', function($scope, session) {
+  controller('ProfileController', function($scope, $http, session) {
     $scope.user = session.user;
-    console.log('user = ', session.user);
+    $scope.actionVerbForActivityType = function(type) {
+      if (type === 'like') {
+        return 'Liked';
+      }
+
+      if (type === 'drink') {
+        return 'Drank';
+      }
+
+      return null;
+    };
+
+    // Fetch user activities
+    $http.get('/api/activities')
+      .then(function(response) {
+        $scope.activities = response.data;
+      })
+      .catch(function(err) {
+        console.log(err);
+      });
   });
