6e400a83b038b1dae19d90a337a173bc3aed656c
Alex Fu <alex.fu914@gmail.com>
2015-10-16 06:32:52 -0400

    Display list of available patches

--- assets/js/controllers/patch.js
+++ assets/js/controllers/patch.js
@@ -1,5 +1,20 @@
 angular.module('stoutful.controllers')
   .controller('PatchController', function($scope, $http) {
+    $scope.onPatchClicked = function(patch) {
+      console.log('Patch clicked = ', patch);
+    };
+
+    $scope.formattedTimestamp = function(timestamp) {
+      var date = moment(timestamp);
+      var format;
+      if (date.year() == moment().year()) {
+        format = 'MMMM Do';
+      } else {
+        format = 'MMMM Do YYYY';
+      }
+      return date.format(format);
+    };
+
     $http.get('/api/patches')
       .then(function(results) {
         $scope.patches = results.data;
