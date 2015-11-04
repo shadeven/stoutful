ebaa83ca21743f6bab2cb38220b9f2434ebab4c3
Alex Fu <alex.fu914@gmail.com>
2015-11-04 00:58:32 -0500

    Implement basic login

--- assets/js/controllers/login.js
+++ assets/js/controllers/login.js
@@ -1,3 +1,30 @@
 angular.module('stoutful.controllers')
-  .controller('LoginController', function($scope) {
+  .controller('LoginController', function($scope, $http, session, basicAuth) {
+    $scope.logIn = function() {
+      if ($scope.loginForm.$invalid) return;
+
+      $scope.loading = true; // Initiate loading animation
+      basicAuth.login($scope.email, $scope.password)
+        .then(function() {
+          return $http.get('/api/users/me');
+        })
+        .then(function(response) {
+          $scope.loading = false;
+          session.user = response.data;
+          $scope.modalInstance.close();
+        })
+        .catch(function(err) {
+          $scope.loading = false;
+          var message = "Unexpected error occurred.";
+          if (err.status === 401) {
+            message = "Email/password incorrect.";
+          }
+          $scope.error = {
+            type: 'danger',
+            msg: message
+          };
+        });
+    };
+
+    $scope.logInWithGoogle = function() {};
   });
--- assets/js/controllers/splash.js
+++ assets/js/controllers/splash.js
@@ -1,4 +1,7 @@
 angular.module('stoutful.controllers').
   controller('SplashController', function($scope) {
     $scope.loginPartial = 'partials/login.html';
+    $scope.showLoginPartial = function() {
+      $scope.showLogin = true;
+    }
   });
