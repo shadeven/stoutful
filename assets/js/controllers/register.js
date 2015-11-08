4f25ee287a51b1158de7fe150cb10c5ddb519fb4
Alex Fu <alex.fu914@gmail.com>
2015-11-07 23:04:44 -0500

    Add register logic

--- assets/js/controllers/register.js
+++ assets/js/controllers/register.js
@@ -1,4 +1,31 @@
 angular.module('stoutful.controllers')
-  .controller('RegisterController', function($scope) {
-    $scope.register = function() {};
+  .controller('RegisterController', function($scope, $http, basicAuth, session) {
+    $scope.register = function() {
+      var data = {
+        first_name: $scope.firstName,
+        last_name: $scope.lastName,
+        email: $scope.email,
+        password: $scope.password
+      };
+
+      $scope.loading = true;
+      $http.post('/api/users/create', data)
+        .then(function() {
+          return basicAuth.login($scope.email, $scope.password);
+        })
+        .then(function() {
+          return $http.get('/api/users/me');
+        })
+        .then(function(response) {
+          $scope.loading = false;
+          session.setUser(response.data);
+        })
+        .catch(function() {
+          $scope.loading = false;
+          $scope.error = {
+            type: 'danger',
+            msg: 'Unexpected error occurred.'
+          };
+        });
+    };
   });
