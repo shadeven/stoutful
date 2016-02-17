6094a20b46f5fd58073dc4d7512fdee443fbd226
Alex Fu <alex.fu914@gmail.com>
2016-02-16 21:14:39 -0500

    Show likes/check in counter in profile

--- assets/js/controllers/profile.js
+++ assets/js/controllers/profile.js
@@ -1,34 +1,64 @@
-angular.module('stoutful.controllers').
-  controller('ProfileController', function($scope, $http, session) {
-    $scope.user = session.user;
+(function() {
+  "use strict";
 
-    if ($scope.user) {
-      loadUserActivities();
+  angular
+    .module("stoutful.controllers")
+    .controller("ProfileController", ProfileController);
+
+  function ProfileController($scope, $http, session) {
+
+    $scope.user = session.user;
+    $scope.actionVerbForActivityType = actionVerbForActivityType;
+    $scope.stats = {};
+
+    loadUserActivities();
+    loadUserStats();
+    watchForSessionChange();
+
+    ////////////////////////////////////////////////////////////////////////////
+
+    function watchForSessionChange() {
+      var target = function() {
+        return session.user;
+      };
+      var listener = function() {
+        $scope.user = session.user;
+        if ($scope.user) {
+          loadUserActivities();
+        }
+      };
+      $scope.$watch(target, listener);
     }
 
-    // Watch for session user change
-    $scope.$watch(function() { return session.user; }, function() {
-      $scope.user = session.user;
-      if ($scope.user) {
-        loadUserActivities();
-      }
-    });
-
-    $scope.actionVerbForActivityType = function(type) {
-      if (type === 'like') {
-        return 'Liked';
+    function actionVerbForActivityType(type) {
+      if (type === "like") {
+        return "Liked";
       }
 
-      if (type === 'check_in') {
-        return 'Checked into';
+      if (type === "check_in") {
+        return "Checked into";
       }
 
       return null;
-    };
+    }
+
+    function loadUserStats() {
+      if (!$scope.user) return;
+
+      $http.get("/api/user/stats")
+        .then(function(response) {
+          $scope.stats = response.data;
+        })
+        .catch(function(err) {
+          console.log(err);
+        });
+    }
 
     function loadUserActivities() {
+      if (!$scope.user) return;
+
       // Fetch user activities
-      $http.get('/api/activities', {params: {'user_id': $scope.user.id}})
+      $http.get("/api/activities", { params: { "user_id": $scope.user.id } })
         .then(function(response) {
           $scope.activities = response.data;
         })
@@ -36,4 +66,5 @@ angular.module('stoutful.controllers').
           console.log(err);
         });
     }
-  });
+  }
+})();
