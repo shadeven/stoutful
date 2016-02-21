d26431a1f7f7147a4602b7368d7407731f42da71
Alex Fu <alex.fu914@gmail.com>
2016-02-21 16:33:04 -0500

    Add env configurations to knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,20 @@
 // Update with your config settings.
 
 module.exports = {
-  client: 'postgresql',
-  connection: {
-    database: 'stoutful',
-    user: 'postgres',
-    host: process.env.POSTGRES_PORT_5432_TCP_ADDR
+  development: {
+    client: "postgresql",
+    connection: {
+      database: "stoutful",
+      user: "postgres",
+      host: process.env.POSTGRES_PORT_5432_TCP_ADDR
+    }
+  },
+  production: {
+    client: "postgresql",
+    connection: {
+      database: "stoutful",
+      user: "postgres",
+      host: process.env.POSTGRES_PORT_5432_TCP_ADDR
+    }
   }
 };
