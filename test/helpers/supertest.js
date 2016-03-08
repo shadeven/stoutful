f8357e45a20c09be2a43866ff2514685cd968f07
Alex Fu <alex.fu914@gmail.com>
2016-03-08 07:45:14 -0500

    Fix supertest helper

--- test/helpers/supertest.js
+++ test/helpers/supertest.js
@@ -1,17 +1,27 @@
+/**
+ * This is a supertest helper that allows us to use `set` methods PRIOR to
+ * creating a request.
+ */
+
 var supertest = require("supertest");
 var config = {};
 
 module.exports = function(sails) {
-  return {
-    agent: supertest(sails.hooks.http.app),
-    get: function(url) {
-      var r = this.agent.get(url);
+  var proxy = {
+    agent: supertest(sails.hooks.http.app)
+  };
+
+  Object.keys(proxy.agent).forEach(function(method) {
+    proxy[method] = function(url) {
+      var r = this.agent[method].call(this, url);
       Object.keys(config).forEach(function(key) {
         r.set(key, config[key]);
       });
       return r;
-    }
-  };
+    };
+  });
+
+  return proxy;
 };
 
 module.exports.set = function(key, value) {
