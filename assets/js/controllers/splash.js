8ac7673e87f6a2bef8fc2e6a038450b0db61e25c
Alex Fu <alex.fu914@gmail.com>
2016-05-21 15:52:26 -0400

    Move gapi auth2 initialization to run block

--- assets/js/controllers/splash.js
+++ assets/js/controllers/splash.js
@@ -20,25 +20,16 @@
     vm.onGoogleSigninSuccess = onGoogleSigninSuccess;
     vm.onGoogleSigninFail = onGoogleSigninFail;
 
-    gapi.load('auth2', function() {
-      // Retrieve the singleton for the GoogleAuth library and set up the client.
-      gapi.auth2.init({
-        client_id: '1068487601849-a0ep88imse3bn202daabmndcni4abhgl.apps.googleusercontent.com',
-        cookiepolicy: 'single_host_origin',
-        scope: 'https://www.googleapis.com/auth/plus.login email'
+    $http({ method: 'GET', url: '/api/users/me' })
+      .then(function(response) {
+        session.setUser(response.data);
+        $location.url('/home');
+      })
+      .catch(function(err) {
+        if (err.status === 401) {
+          session.destroy();
+        }
       });
-
-      $http({ method: 'GET', url: '/api/users/me' })
-        .then(function(response) {
-          session.setUser(response.data);
-          $location.url('/home');
-        })
-        .catch(function(err) {
-          if (err.status === 401) {
-            session.destroy();
-          }
-        });
-    });
 
     ////////////////////////////////////////////////////////////////////////////
 
