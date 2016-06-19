a228f60a5b118bc657d1ea08d537b5a4b2729155
Alex Fu <alex.fu914@gmail.com>
2016-06-19 18:47:33 -0400

    Create RefreshToken if needed

--- api/services/Oauth2.js
+++ api/services/Oauth2.js
@@ -1,4 +1,4 @@
-/* global User, AccessToken */
+/* global User, AccessToken, RefreshToken */
 
 var oauth2orize = require("oauth2orize");
 var chain = require("connect-chain");
@@ -9,13 +9,21 @@ server.exchange(oauth2orize.exchange.password(function(client, username, passwor
   User.findOne({ email: username })
     .then(function(user) {
       if (!user) return done(null, false);
-
       user.verifyPassword(password)
         .then(function(match) {
           if (!match) return done(null, false);
-          AccessToken.generateAndSave(user.id)
-            .then(function(accessToken) {
-              done(null, accessToken.token);
+          RefreshToken.findOne({user_id: user.id})
+            .then(function(refreshToken) {
+              if (!refreshToken) return RefreshToken.generateAndSave(user.id);
+              return refreshToken;
+            })
+            .then(function(refreshToken) {
+              AccessToken.generateAndSave(user.id)
+                .then(function(accessToken) {
+                  done(null, accessToken.token, refreshToken.token, {
+                    expires_in: accessToken.expiresIn()
+                  });
+                });
             });
         });
     });
