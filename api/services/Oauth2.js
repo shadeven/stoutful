095bf398b1073e2ddc8896e7cd1d195e0597ecf1
Alex Fu <alex.fu914@gmail.com>
2016-06-19 13:38:54 -0400

    Handle OAuth2 exchange

--- api/services/Oauth2.js
+++ api/services/Oauth2.js
@@ -1,10 +1,24 @@
+/* global User, AccessToken */
+
 var oauth2orize = require("oauth2orize");
 var chain = require("connect-chain");
 
 var server = oauth2orize.createServer();
 
 server.exchange(oauth2orize.exchange.password(function(client, username, password, scope, done) {
-  // TODO: Handle
+  User.findOne({ email: username })
+    .then(function(user) {
+      if (!user) return done(null, false);
+
+      user.verifyPassword(password)
+        .then(function(match) {
+          if (!match) return done(null, false);
+          AccessToken.generateAndSave(user.id)
+            .then(function(accessToken) {
+              done(null, accessToken);
+            });
+        });
+    });
 }));
 
 module.exports.token = chain(server.token(), server.errorHandler());
