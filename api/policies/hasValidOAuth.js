77a54c8afcad5314f189a5a84cbe66a690162959
Alex Fu <alex.fu914@gmail.com>
2016-06-19 18:53:22 -0400

    Rename Oauth2 service

--- api/policies/hasValidOAuth.js
+++ api/policies/hasValidOAuth.js
@@ -3,7 +3,7 @@ var chain = require("connect-chain");
 module.exports = function(req, res, next) {
   var middleware = chain(
     Passport.authenticate("basic", {session: false}),
-    Oauth2.token
+    OAuth2.token
   );
   middleware(req, res, next);
 };
